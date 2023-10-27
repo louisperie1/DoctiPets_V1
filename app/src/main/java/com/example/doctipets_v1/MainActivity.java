d38ee3bb5993bc42ad8a686c2437861578e2e582
Louis_PERIE <louis.perie-riffes@edu.esiee.fr>
2023-10-27 15:31:19 +0200

    Initial commit

---
+++ app/src/main/java/com/example/doctipets_v1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.doctipets_v1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //Button boutonRecherche = findViewById(R.id.boutonRecherche);
+    }
